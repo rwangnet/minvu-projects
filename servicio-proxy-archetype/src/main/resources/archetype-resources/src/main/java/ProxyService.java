dce3f5a3816f7933cf32f90bca86f272368882b5
rwangnet <rwangnet@gmail.com>
2015-05-29 16:21:52 -0400

    Hola

---
+++ servicio-proxy-archetype/src/main/resources/archetype-resources/src/main/java/ProxyService.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * 
+ */
+package ${package};
+
+/**
+ * @author rwangnet
+ *
+ */
+public interface ProxyService {
+
+}
---
+++ servicio-proxy-archetype/src/main/resources/archetype-resources/src/main/java/Reference.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * 
+ */
+package ${package};
+
+/**
+ * @author rwangnet
+ * Interfaz de Componente Base referenciado expuesto via REST
+ */
+public interface Reference {
+
+}
