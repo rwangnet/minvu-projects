917840c353de1e0d0c8707f3e3775f16374e49c3
rwangnet <rwangnet@gmail.com>
2015-06-17 23:50:27 -0300

    Componente Base de negocio (JPA agnostico + EJB)

---
+++ componente-negocio/src/main/java/cl/minvu/negocio/PersonaControllerImpl.java
@@ -0,0 +1,40 @@
+package cl.minvu.negocio;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import cl.minvu.dao.PersonaDAOInterface;
+import cl.minvu.entities.Persona;
+/*
+ * Implementación de los típicos métodos de operaciones CRUD definidos en la interfaz para operaciones de negocio
+ * sobre la información de personas almacenadas en cualquier DBMS.
+ */
+
+@Stateless
+public class PersonaControllerImpl implements PersonaControllerInterface {
+
+	@Inject
+	PersonaDAOInterface personaDao;
+	
+	@Override
+	public void guardarPersona(Persona persona) {
+	personaDao.guardarPersona(persona);
+
+	}
+
+	@Override
+	public void eliminarPersona(Integer _id) {
+    personaDao.eliminarPersona(_id);
+	}
+
+	@Override
+	public void modificarPersona(Persona persona) {
+    personaDao.modificarPersona(persona);
+	}
+
+	@Override
+	public Persona obtenerPersona(Integer _id) {
+	return personaDao.obtenerPersona(_id);
+	}
+
+}
---
+++ servicio-negocio-ejemplo/src/main/java/cl/minvu/negocio/ejemplo/PersonaServiceInterface.java
@@ -0,0 +1,9 @@
+package cl.minvu.negocio.ejemplo;
+
+import cl.minvu.negocio.ejemplo.TO.PersonaTO;
+
+public interface PersonaServiceInterface {
+
+	public PersonaTO obtiene(PersonaTO persona);
+	
+}
